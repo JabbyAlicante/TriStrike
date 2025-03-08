e49670023e587479f71499ad2f4fc5793ea94d54
geripineda <gerigreizelle@gmail.com>
2025-03-08 11:59:49 +0800

    media queries

--- frontend1/src/pages/landing_page.jsx
+++ frontend1/src/pages/landing_page.jsx
@@ -1,7 +1,7 @@
 import LandingPage from '../components/landing.js';
 import Layout from '../layouts/default.js';
 
-export default function Home() {
+export default function Landing() {
   const { main } = Layout(this.root);
 
   LandingPage(main);
